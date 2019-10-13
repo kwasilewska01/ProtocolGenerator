503e5900aac72ee8bfa696df72c1d42ffb1b5604
kwasilewska01 <k.wasilewska01@gmail.com>
2019-10-13 21:15:11 +0200

    first push, install json db

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import Collapse from './components/ui/Collapse';
+
+function App() {
+  return (
+    <Collapse />
+  );
+}
+
+export default App;
---
+++ src/components/ui/Collapse.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Collapse.scss';
+
+export default function App() {
+  return (
+    <div class="row">
+        <div class="col-1">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Dashbord</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Osoby</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Firmy</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Konrakty</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Raporty</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Ustawienia</a>
+            </div>
+        </div>
+    </div>
+ );
+}
