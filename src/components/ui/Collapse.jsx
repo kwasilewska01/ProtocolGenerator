0d5731168908abc602a74cb2bdd93dbd451e33f4
kwasilewska01 <k.wasilewska01@gmail.com>
2019-10-13 23:10:38 +0200

    added single squer to left nav

--- src/components/ui/Collapse.jsx
+++ src/components/ui/Collapse.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Collapse.scss';
+import Squere from './Squere';
 
-export default function App() {
-  return (
-    <div class="row">
-        <div class="col-1">
-            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Dashbord</a>
-                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Osoby</a>
-                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Firmy</a>
-                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Konrakty</a>
-                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Raporty</a>
-                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Ustawienia</a>
+export default class Collapse extends Component {
+    render(){
+    return (
+        <div class="row">
+            <div class="col-1">
+                <div class="nav nav-pills" role="tablist" aria-orientation="vertical">
+                    <Squere 
+                        tabText="Josi"
+                        />
+                    {/* <a className="nav-link" data-toggle="pill" href="#v-pills-home" role="tab" aria-selected="false">Dashbord</a>
+                    
+                    <a className="nav-link" data-toggle="pill" href="#v-pills-profile" role="tab" aria-selected="false">Osoby</a>
+                    <a className="nav-link" data-toggle="pill" href="#v-pills-messages" role="tab" aria-selected="false">Firmy</a>
+                    <a className="nav-link" data-toggle="pill" href="#v-pills-settings" role="tab" aria-selected="false">Konrakty</a>
+                    <a className="nav-link" data-toggle="pill" href="#v-pills-settings" role="tab" aria-selected="false">Raporty</a>
+                    <a className="nav-link"  data-toggle="pill" href="#v-pills-settings" role="tab" aria-selected="false">Ustawienia</a> */}
+                </div>
             </div>
         </div>
-    </div>
- );
+    );
+    }
 }
